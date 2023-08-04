64cf27e86835265e97f3e2fa06f41a97224e0e99
Yossef Mohamed <129265865+YossefAbdelMohsen@users.noreply.github.com>
2023-08-04 23:38:19 +0300

    Update cdn of bootstrap

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -40,7 +40,7 @@ export const Layout = ({ children }) => {
         />
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.4/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossOrigin="anonymous"
         />
